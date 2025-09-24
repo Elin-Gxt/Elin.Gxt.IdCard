9cd829870180e0e6a15a1524e230d80bb7bab3e2
Steve Biedermann <hardliner66@gmail.com>
2025-09-24 16:38:47 +0200

    rename to Elin.Gxt.IdCard

--- Plugin.cs
+++ Plugin.cs
@@ -1,19 +1,35 @@
 using BepInEx;
 using BepInEx.Configuration;
-using Gxt.Net;
-using Gxt.Net.Advisory;
+using Cwl.API.Attributes;
+using Cwl.API.Processors;
+using Elin.Gxt;
+using Elin.Gxt.Advisory;
 using HarmonyLib;
+using Newtonsoft.Json;
+using ReflexCLI.Attributes;
+using Steamworks;
 using System;
+using System.Diagnostics;
 using System.IO;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using UnityEngine;
 
 namespace MyMod;
 public static class ModInfo
 {
     public const string Guid = "iah.gxt.IdCard";
-    public const string Name = "GxtIdCard";
+    public const string Name = "GXT ID Card";
     public const string Version = "1.0.0";
+}
+
+class IdCardMeta
+{
+    public string CharacterName { get; set; } = Game.pc.NameSimple;
+    public string SteamName { get; set; } = SteamFriends.GetPersonaName();
+    public string Portrait { get; set; } = Game.pc.GetIdPortrait();
+    public int CurrentLevel { get; set; } = Game.pc.LV;
+    public string Race { get; set; } = Game.pc.race.GetName();
 }
 
 class KeyWrapper
@@ -22,23 +38,27 @@ class KeyWrapper
     public string IdCard { get; set; }
 }
 
+[HarmonyPatch(typeof(Game), nameof(Game.StartNewGame))]
+class StartNewGamePatch
+{
+    static void Postfix()
+    {
+        Plugin.MakeIdentity(KeyCreation.WithKey);
+    }
+}
+
 [HarmonyPatch(typeof(Game), nameof(Game.OnLoad))]
 class LoadGamePatch
 {
     static void Postfix()
     {
-        Plugin.KeyPath = Path.Combine(GameIO.pathCurrentSave, "key.gxk");
-        Plugin.IdCardPath = Path.Combine(GameIO.pathCurrentSave, "id.gxi");
+        Plugin.SetSavePaths();
 
         if (!File.Exists(Plugin.KeyPath))
         {
             try
             {
-                var _key = GxtSdk.MakeKey();
-                var _idCard = GxtSdk.MakeIdCard(_key, new IdCard { DisplayName = Game.pc.Name });
-                Plugin.Identity = new KeyWrapper { Key = _key, IdCard = _idCard };
-                File.WriteAllText(Plugin.KeyPath, _key, System.Text.Encoding.UTF8);
-                File.WriteAllText(Plugin.IdCardPath, _idCard, System.Text.Encoding.UTF8);
+                Plugin.MakeIdentity(KeyCreation.WithKey);
             }
             catch (FfiException e)
             {
@@ -51,10 +71,7 @@ class LoadGamePatch
         {
             try
             {
-                var _key = File.ReadAllText(Plugin.IdCardPath);
-                var _idCard = GxtSdk.MakeIdCard(_key, new IdCard { DisplayName = Game.pc.Name });
-                Plugin.Identity = new KeyWrapper { Key = _key, IdCard = _idCard };
-                File.WriteAllText(Plugin.IdCardPath, _idCard, System.Text.Encoding.UTF8);
+                Plugin.MakeIdentity(KeyCreation.WithoutKey);
             }
             catch (FfiException e)
             {
@@ -69,6 +86,12 @@ class LoadGamePatch
     }
 }
 
+enum KeyCreation
+{
+    WithKey,
+    WithoutKey,
+}
+
 [BepInPlugin(ModInfo.Guid, ModInfo.Name, ModInfo.Version)]
 internal class Plugin : BaseUnityPlugin
 {
@@ -76,6 +99,67 @@ internal class Plugin : BaseUnityPlugin
     internal static KeyWrapper Identity = null;
     internal static string KeyPath = "";
     internal static string IdCardPath = "";
+
+    internal static void SaveKey()
+    {
+        File.WriteAllText(KeyPath, Identity.Key, System.Text.Encoding.ASCII);
+    }
+
+    internal static void SaveIdCard()
+    {
+        File.WriteAllText(IdCardPath, Identity.IdCard, System.Text.Encoding.ASCII);
+    }
+
+    internal static void SetSavePaths()
+    {
+        KeyPath = Path.Combine(GameIO.pathCurrentSave, "key.gxk");
+        IdCardPath = Path.Combine(GameIO.pathCurrentSave, "id.gxi");
+    }
+
+    internal static void MakeIdentity(KeyCreation withKey)
+    {
+        SetSavePaths();
+        try
+        {
+            string key;
+            if (withKey == KeyCreation.WithKey)
+            {
+                key = GxtSdk.MakeKey();
+            }
+            else
+            {
+                key = Identity.Key;
+            }
+            var idCard = GxtSdk.MakeIdCard(key, new IdCardMeta());
+            Identity = new KeyWrapper { Key = key, IdCard = idCard };
+            SaveKey();
+            SaveIdCard();
+        }
+        catch (FfiException e)
+        {
+            LogError($"GXT Error Code: {e.ErrorCode}");
+        }
+
+    }
+
+    [CwlContextMenu("gxt_ui_id_card_show")]
+    internal static void ShowIdCard()
+    {
+        // Hacky, but works for now
+        Process.Start("notepad.exe", $"\"{IdCardPath}\"");
+    }
+
+    [CwlContextMenu("gxt_ui_id_card_copy")]
+    internal static void CopyIdToClipboard()
+    {
+        GUIUtility.systemCopyBuffer = Identity.IdCard;
+    }
+
+    public static void Say(string text)
+    {
+        Msg.SetColor(Msg.colors.TalkGod);
+        Msg.Say(text);
+    }
 
     private void Awake()
     {
@@ -88,13 +172,13 @@ internal class Plugin : BaseUnityPlugin
         Instance?.Logger.LogDebug($"[{caller}] {message}");
     }
 
-    internal static void LogInfo(object message)
+    internal static void LogInfo(object message, [CallerMemberName] string caller = "")
     {
-        Instance?.Logger.LogInfo(message);
+        Instance?.Logger.LogInfo($"[{caller}] {message}");
     }
 
-    internal static void LogError(object message)
+    internal static void LogError(object message, [CallerMemberName] string caller = "")
     {
-        Instance?.Logger.LogError(message);
+        Instance?.Logger.LogError($"[{caller}] {message}");
     }
 }
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# GxtIdCard
+# Elin.Gxt.IdCard
 
-Mod to allow creating GXT ID Cards for use with other GXT based mods.
+Mod to allow creating [GXT](https://github.com/game-exchange-token/gxt) ID Cards for use with other GXT based mods.
