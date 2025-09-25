cafe85d02f06831bfc1c4efc50007780beeb9ba3
Steve Biedermann <hardliner66@gmail.com>
2025-09-25 08:30:38 +0200

    working base

--- Plugin.cs
+++ Plugin.cs
@@ -1,21 +1,22 @@
 using BepInEx;
-using BepInEx.Configuration;
 using Cwl.API.Attributes;
 using Cwl.API.Processors;
 using Elin.Gxt;
-using Elin.Gxt.Advisory;
 using HarmonyLib;
-using Newtonsoft.Json;
-using ReflexCLI.Attributes;
 using Steamworks;
 using System;
-using System.Diagnostics;
-using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using UnityEngine;
+using YKF;
 
 namespace MyMod;
+
+using static Widget;
+using IdCards = Dictionary<string, GxtIdentity>;
+
 public static class ModInfo
 {
     public const string Guid = "iah.gxt.IdCard";
@@ -23,136 +24,229 @@ public static class ModInfo
     public const string Version = "1.0.0";
 }
 
-class IdCardMeta
+public class ElinIdCardMeta
 {
-    public string CharacterName { get; set; } = Game.pc.NameSimple;
-    public string SteamName { get; set; } = SteamFriends.GetPersonaName();
-    public string Portrait { get; set; } = Game.pc.GetIdPortrait();
-    public int CurrentLevel { get; set; } = Game.pc.LV;
-    public string Race { get; set; } = Game.pc.race.GetName();
-}
+    public string CharacterName { get; }
+    public string SteamName { get; }
+    public string Portrait { get; }
+    public int CurrentLevel { get; }
+    public string Race { get; }
 
-class KeyWrapper
-{
-    public string Key { get; set; }
-    public string IdCard { get; set; }
-}
-
-[HarmonyPatch(typeof(Game), nameof(Game.StartNewGame))]
-class StartNewGamePatch
-{
-    static void Postfix()
+    public ElinIdCardMeta(string characterName, string steamName, string portrait, int currentLevel, string race)
     {
-        Plugin.MakeIdentity(KeyCreation.WithKey);
+        CharacterName = characterName;
+        SteamName = steamName;
+        Portrait = portrait;
+        CurrentLevel = currentLevel;
+        Race = race;
+    }
+
+    public string FullName()
+    {
+        return $"{CharacterName} ({SteamName})";
     }
 }
 
-[HarmonyPatch(typeof(Game), nameof(Game.OnLoad))]
-class LoadGamePatch
+class IdCardNote : Thing
 {
-    static void Postfix()
+    ElinIdCardMeta _Meta;
+    public IdCardNote(ElinIdCardMeta meta) { _Meta = meta; }
+
+    public override string GetName(NameStyle style, int num = -1) { return "ID Card"; }
+
+    public override void WriteNote(UINote n, Action<UINote>? onWriteNote = null, IInspect.NoteMode mode = IInspect.NoteMode.Default, Recipe? recipe = null)
     {
-        Plugin.SetSavePaths();
+        n.Clear();
+        n.AddHeader($"Player Name: {_Meta.SteamName}");
+        n.AddText($"Character Name: {_Meta.CharacterName}");
+        n.AddText($"Level: {_Meta.CurrentLevel}");
+        n.AddText($"Race: {_Meta.Race}");
 
-        if (!File.Exists(Plugin.KeyPath))
-        {
-            try
-            {
-                Plugin.MakeIdentity(KeyCreation.WithKey);
-            }
-            catch (FfiException e)
-            {
-                Plugin.LogError(e.ErrorCode);
-            }
-            return;
-        }
-
-        if (!File.Exists(Plugin.IdCardPath))
-        {
-            try
-            {
-                Plugin.MakeIdentity(KeyCreation.WithoutKey);
-            }
-            catch (FfiException e)
-            {
-                Plugin.LogError(e.ErrorCode);
-            }
-            return;
-        }
-
-        var key = File.ReadAllText(Plugin.KeyPath);
-        var idCard = File.ReadAllText(Plugin.IdCardPath);
-        Plugin.Identity = new KeyWrapper { Key = key, IdCard = idCard };
+        var portrait = Portrait.modPortraits.dict[_Meta.Portrait];
+        n.AddImage(portrait.GetObject());
     }
 }
 
-enum KeyCreation
+class AddressBookContactsTab : YKLayout<IdCards>
 {
-    WithKey,
-    WithoutKey,
+    private int _Current = 0;
+    private List<GxtIdentity> _Values = new();
+    private List<string> CreateOptions()
+    {
+        return _Values.Select(id => id.Meta.FullName()).ToList();
+    }
+    public override void OnLayout()
+    {
+        _Values = [.. Layer.Data.Values];
+        var d = Dropdown(CreateOptions(), i => _Current = i, _Current);
+        var h = Horizontal();
+        h.Button("Open", () =>
+        {
+            SE.Rotate();
+            EClass.ui.AddLayer<LayerInfo>().Set(new IdCardNote(_Values[_Current].Meta));
+        });
+
+        h.Button("My ID Card", () =>
+        {
+            SE.Rotate();
+            EClass.ui.AddLayer<LayerInfo>().Set(new IdCardNote(Plugin.IdCardMeta()));
+        });
+
+        h.Button("Delete", () =>
+        {
+            var id = _Values[_Current].VerificationKey;
+            Plugin.Contacts.Remove(id);
+            _Values.RemoveAll(identity => identity.VerificationKey == id);
+            d.options = CreateOptions().ToDropdownOptions();
+        });
+    }
+}
+
+class AddressBook : YKLayer<IdCards>
+{
+    public override void OnLayout()
+    {
+        CreateTab<AddressBookContactsTab>("Contacts", "");
+    }
+}
+
+public class GxtIdentity
+{
+    public string VerificationKey { get; set; }
+    public string EncryptionKey { get; set; }
+    public ElinIdCardMeta Meta { get; set; }
+    public GxtIdentity(Envelope<ElinIdCardMeta> envelope)
+    {
+        VerificationKey = envelope.verification_key;
+        EncryptionKey = envelope.encryption_key;
+        Meta = envelope.payload;
+    }
 }
 
 [BepInPlugin(ModInfo.Guid, ModInfo.Name, ModInfo.Version)]
 internal class Plugin : BaseUnityPlugin
 {
     internal static Plugin? Instance;
-    internal static KeyWrapper Identity = null;
-    internal static string KeyPath = "";
-    internal static string IdCardPath = "";
+    public static Dictionary<string, GxtIdentity> Contacts = new Dictionary<string, GxtIdentity>();
 
-    internal static void SaveKey()
+    const string PRIVATE_KEY_CHUNK = "Elin.Gxt.PrivateKey";
+    const string ADDRESS_BOOK_CHUNK = "Elin.Gxt.AddressBook";
+    static string PrivateKey = GxtSdk.MakeKey();
+
+    [CwlPreSave]
+    static void MySaveHandler(GameIOProcessor.GameIOContext context)
     {
-        File.WriteAllText(KeyPath, Identity.Key, System.Text.Encoding.ASCII);
+        if (string.IsNullOrWhiteSpace(PrivateKey))
+        {
+            PrivateKey = GxtSdk.MakeKey();
+        }
+        context.Save(PrivateKey, PRIVATE_KEY_CHUNK);
+
+        if (Plugin.Contacts is null)
+        {
+            Plugin.Contacts = new Dictionary<string, GxtIdentity>();
+        }
+        context.Save(Plugin.Contacts, ADDRESS_BOOK_CHUNK);
     }
 
-    internal static void SaveIdCard()
+    [CwlPostLoad]
+    static void MyLoadHandler(GameIOProcessor.GameIOContext context)
     {
-        File.WriteAllText(IdCardPath, Identity.IdCard, System.Text.Encoding.ASCII);
+        if (!context.Load<string>(out PrivateKey!, PRIVATE_KEY_CHUNK) || PrivateKey is null || string.IsNullOrWhiteSpace(PrivateKey))
+        {
+            PrivateKey = GxtSdk.MakeKey();
+        }
+
+        if (!context.Load<Dictionary<string, GxtIdentity>>(out Plugin.Contacts!, ADDRESS_BOOK_CHUNK) || Plugin.Contacts is null)
+        {
+            Plugin.Contacts = new Dictionary<string, GxtIdentity>();
+        }
     }
 
-    internal static void SetSavePaths()
+
+    private static string GetSteamName()
     {
-        KeyPath = Path.Combine(GameIO.pathCurrentSave, "key.gxk");
-        IdCardPath = Path.Combine(GameIO.pathCurrentSave, "id.gxi");
+        if (Steam.Instance == null)
+        {
+            return "N/A";
+        }
+
+        return SteamFriends.GetPersonaName();
     }
 
-    internal static void MakeIdentity(KeyCreation withKey)
+    public static ElinIdCardMeta IdCardMeta()
     {
-        SetSavePaths();
+        return new ElinIdCardMeta(
+             Game.pc.NameSimple,
+             GetSteamName(),
+             Game.pc.GetIdPortrait(),
+             Game.pc.LV,
+             Game.pc.race.GetName()
+        );
+    }
+
+
+    internal static string MakeIdCard()
+    {
+        return GxtSdk.MakeIdCard(PrivateKey, IdCardMeta());
+    }
+
+    [CwlContextMenu("GXT/gxt_ui_address_book_show")]
+    internal static void ShowAddressBook()
+    {
+        SE.Rotate();
+        YK.CreateLayer<AddressBook, IdCards>(Plugin.Contacts);
+    }
+
+    [CwlContextMenu("GXT/gxt_ui_id_card_copy")]
+    internal static void CopyIdToClipboard()
+    {
+        GUIUtility.systemCopyBuffer = MakeIdCard();
+        Say("ID Card copied to Clipboard");
+    }
+
+    [CwlContextMenu("GXT/gxt_ui_id_card_import")]
+    internal static void ContextMenuImportFromClipboard()
+    {
         try
         {
-            string key;
-            if (withKey == KeyCreation.WithKey)
+            ImportFromClipboard();
+        }
+        catch (Exception e)
+        {
+            Say("Failed to import identity. Make sure you have an ID Card token is in your clipboard!");
+            LogError($"Failed to import identity: {e.Message}");
+        }
+    }
+
+    internal static void ImportFromClipboard(Action<GxtIdentity>? onNewIdentity = null)
+    {
+        var id_card_token = GUIUtility.systemCopyBuffer;
+        var envelope = GxtSdk.VerifyMessage<ElinIdCardMeta>(id_card_token);
+        var id = envelope.verification_key;
+        if (Contacts.ContainsKey(id))
+        {
+            Contacts[id].Meta = envelope.payload;
+            Contacts[id].EncryptionKey = envelope.encryption_key;
+            return;
+        }
+        try
+        {
+            var identity = new GxtIdentity(envelope);
+            if (onNewIdentity is not null)
             {
-                key = GxtSdk.MakeKey();
+                onNewIdentity(identity);
             }
-            else
-            {
-                key = Identity.Key;
-            }
-            var idCard = GxtSdk.MakeIdCard(key, new IdCardMeta());
-            Identity = new KeyWrapper { Key = key, IdCard = idCard };
-            SaveKey();
-            SaveIdCard();
+            Contacts.Add(id, identity);
         }
         catch (FfiException e)
         {
-            LogError($"GXT Error Code: {e.ErrorCode}");
+            Say($"Failed to import the ID Card. Error: {e.ErrorCode}");
         }
-
-    }
-
-    [CwlContextMenu("gxt_ui_id_card_show")]
-    internal static void ShowIdCard()
-    {
-        // Hacky, but works for now
-        Process.Start("notepad.exe", $"\"{IdCardPath}\"");
-    }
-
-    [CwlContextMenu("gxt_ui_id_card_copy")]
-    internal static void CopyIdToClipboard()
-    {
-        GUIUtility.systemCopyBuffer = Identity.IdCard;
+        catch (Exception e)
+        {
+            Say($"Failed to import the ID Card. {e.Message}");
+        }
     }
 
     public static void Say(string text)
